80a3a6ff6ddd5ad14c45d079230226bc282b67f1
sahilsharma <sahilsharma75831@gmail.com>
2023-07-10 08:03:25 +0530

    firebase.js updated

--- Firebase.js
+++ Firebase.js
@@ -4,8 +4,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  // Your web app's Firebase configuration
+  apiKey: "AIzaSyAVnR9mIZcDw8xCps2NG_iYQxOA_L2gOMU",
+  authDomain: "sahilchatapp-749c4.firebaseapp.com",
+  projectId: "sahilchatapp-749c4",
+  storageBucket: "sahilchatapp-749c4.appspot.com",
+  messagingSenderId: "6774823448",
+  appId: "1:6774823448:web:0c2a5751a873c0ee2e416d"
 };
 
 // Initialize Firebase
